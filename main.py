12572b22ea0d855e3e9ad1e0e280a95d7c156c6e
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2023-04-19 19:36:02 +0500

    Commit #3

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, SelectField, EmailField
+from wtforms.validators import DataRequired, Optional
+
+
+class UserForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired(message="Поле не должо быть пустым")])
+    text = TextAreaField('Отзыв', validators=[DataRequired(message="Поле не должо быть пустым")])
+    email = EmailField('Почта', validators=[Optional()])
+    rating = SelectField('Оценка', choices=[1, 2, 3, 4, 5], default=5)
+    submit = SubmitField('Отправить')
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET KEY'
 
 news_list = [
     {"title": "Новость 1", "text": "текст для ноовости 1"},
@@ -12,6 +25,22 @@ news_list = [
 
 def index():
     return render_template("index.html", news_list=news_list)
+
+
+def form():
+    user_form = UserForm()
+
+    if user_form.validate_on_submit():
+        name = user_form.name.data
+        text = user_form.text.data
+        email = user_form.email.data
+        rating = user_form.rating.data
+
+        print(name, text, email, rating, sep="\n--------------\n")
+
+        return redirect("/")
+
+    return render_template("form.html", form=user_form)
 
 
 def news():
@@ -27,6 +56,7 @@ def category(name):
 
 
 app.add_url_rule('/', 'index', index)
+app.add_url_rule('/form', 'form', form, methods=["GET", "POST"])
 app.add_url_rule('/news', 'news', news)
 app.add_url_rule('/news_detail/<int:id>', 'news_detail', news_detail)
 app.add_url_rule('/category/<string:name>', 'category', category)
